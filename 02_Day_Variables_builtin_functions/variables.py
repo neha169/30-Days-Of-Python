a16accfe01701aeef00fc3c4920e1c00ce9c9fdc
neha169 <nehanirmal016@gmail.com>
2026-06-18 12:21:52 +0530

    Day 2: 30 Days of python programming

---
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,55 @@
+first_name = "Neha"
+last_name = "Nirmal"
+full_name = "Neha Nirmal"
+country = "India"
+city = "Mumbai"
+age = 24
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("Country:", country)
+print("City:", city)
+print("Age:", age)
+print("Year:", year)
+print("Is Married:", is_married)
+print("Is True:", is_true)
+print("Is Light On:", is_light_on)
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))    
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4 
+total = num_one + num_two
+print("Total:", total)
+diff = num_one - num_two
+print("Subtraction:", diff)
+product = num_one * num_two  
+print("Multiplication:", product)
+division = num_one / num_two
+print("Division:", division)    
+floor_division = num_one // num_two
+print("Floor Division:", floor_division)
+reminder = num_one % num_two
+print("Modulus:", reminder)
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+print("Area of Circle:", area_of_circle)
+circum_of_circle = 2 * 3.14 * radius
+print("Circumference of Circle:", circum_of_circle)
