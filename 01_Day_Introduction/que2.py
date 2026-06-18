a413db03d89b65753862e439abea24938dc7f939
neha169 <nehanirmal016@gmail.com>
2026-06-18 11:54:49 +0530

    rename

---
+++ 01_Day_Introduction/que2.py
@@ -0,0 +1,16 @@
+"""Open the python interactive shell and do the following operations. The operands are 3 and 4.
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//)
+"""
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3//4)
+print(3%4)
+print(3**4)
---
+++ 01_Day_Introduction/que3.py
@@ -0,0 +1,11 @@
+"""
+Write strings on the python interactive shell. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+"""
+print("Neha")
+print("Nirmal")
+print("India")
+print("I am enjoying 30 days of python")
